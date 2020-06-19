db048c1a214d5efd2b8d2c858d3307fe4ef44fe8
Andrew Pomeroy <andrew_pomeroy@windsorsolutions.com>
2020-06-19 11:43:01 -0700

    revamp multi-role role/invite display at summary, add "Cancel signing" option

--- src/signing-pages/signing-setup-summary.js
+++ src/signing-pages/signing-setup-summary.js
@@ -20,6 +20,8 @@ function SigningSetupSummaryCtrl($window, $mdDialog) {
 	$ctrl.$onInit = function () {
 		$ctrl.roles = angular.copy($ctrl.wndModel.model.data.signingRoles);
 
+		initSelectedModel();
+
 		// just for mockup purposes!
 		$ctrl.roles[$ctrl.roles.length - 1].isSigned = true;
 		$ctrl.invites[$ctrl.invites.length - 1].isMissingEsigRole = true;
@@ -33,8 +35,36 @@ function SigningSetupSummaryCtrl($window, $mdDialog) {
 		},
 		isSelfSigned: {
 			get: () => $ctrl.wndModel.model.data.isSelfSigned
-		}
+		},
+		entries: {
+			get: () => $ctrl.roles && $ctrl.roles.length
+				? $ctrl.roles
+				: $ctrl.invites
+		},
 	});
+
+
+	$ctrl.viewRoles = function () {
+		$ctrl.viewingInvites = false;
+	}
+	$ctrl.viewInvites = function () {
+		$ctrl.viewingInvites = true;
+	}
+
+	$ctrl.multipleRolesAvailable = () => $ctrl.roles.filter(x => !x.isSigned).length > 1;
+
+	$ctrl.toggleMultiSign = () => {
+		initSelectedModel();
+		$ctrl.isMultiSigningActive = !$ctrl.isMultiSigningActive
+	};
+
+	$ctrl.toggleRole = (role) => {
+		return $ctrl.isRoleSelected(role) ? $ctrl.deselectRole(role) : $ctrl.selectRole(role);
+	}
+
+	function initSelectedModel () {
+		$ctrl.selectedModel = $ctrl.roles.map(x => false)
+	}
 
 	$ctrl.selectRole = role => {
 		$ctrl.selectedRoles.push(role);
@@ -44,7 +74,18 @@ function SigningSetupSummaryCtrl($window, $mdDialog) {
 		$ctrl.selectedRoles = $ctrl.selectedRoles.filter(x => x.name !== role.name);
 	};
 
-	$ctrl.isRoleSelected = role => $ctrl.selectedRoles.find(x => x.name === role.name);
+	// $ctrl.isRoleSelected = role => {
+	// 	console.log($ctrl.);
+	// 	var ok =  $ctrl.selectedRoles.find(x => x.name === role.name);
+	// 	console.log(ok);
+	// 	return ok;
+	// }
+
+	$ctrl.isAnyRoleSelected = () => $ctrl.selectedModel.some(x => x);
+
+	$ctrl.doMultipleSign = $event => {
+		$ctrl.openSigningDialog($event, $ctrl.roles.filter((role, index) => $ctrl.selectedModel[index]))
+	}
 
 	$ctrl.openSigningDialog = ($event, role) => {
 		$ctrl.signingWizard.doSigning($event, role).then(result => {
